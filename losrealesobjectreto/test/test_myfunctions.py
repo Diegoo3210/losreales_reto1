d4111acb89e9e52afcd00eb73b56f932ca918880
jmarinm <j.marinm@uniandes.edu.co>
2023-06-03 13:45:28 -0500

    fix

---
+++ losrealesobjectreto/test/test_myfunctions.py
@@ -0,0 +1,4 @@
+def detect_objects_in_video(video_path, output_path):
+    return True
+def get_frame_info(frame):
+    return True
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='losrealesobjectreto',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This library aims to approach solving specialized image object recognition in the Amazonas region.',
+    author='Grupo los reales',
+    license='MIT',
+)
